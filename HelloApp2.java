69738a4fb5a565c44ad2e9ae7e2a9a76a394ce56
Steven <srammos1@gmail.com>
2023-02-15 20:15:25 +0000

    inserted sayHello()

--- HelloApp2.java
+++ HelloApp2.java
@@ -1,5 +1,11 @@
+/*
+ * create a myGreeterObject that can hold objects created from the Greeter class
+ */
 public class HelloApp2 {
+    public  static void main(String[] args){
+        Greeter myGreeterObject = new Greeter();
 
+    } 
 
 
     
