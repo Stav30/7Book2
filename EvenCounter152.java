dd81ca1cdf51fd3e82ffe2128e4131bb452305a4
Steven <srammos1@gmail.com>
2023-02-16 18:20:58 +0000

    while loop to print even numbers <= 20

---
+++ EvenCounter152.java
@@ -0,0 +1,14 @@
+/*
+ * while loop to print even numbers <= 20
+ */
+public class EvenCounter152 {
+    public static void main(String[] args){
+        int number = 2;
+
+        while (number <= 20){
+            System.out.print(number + " ");
+            number += 2;
+        }
+
+    }
+}
---
+++ GetInteger222.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public class GetInteger222 {
+    static Scanner sc = new Scanner(System.in);
+
+}
